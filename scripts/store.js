0bc34c0105fe4c80fa1cb5805369682167384a92
1harold0 <harolddiaz.139@gmail.com>
2024-06-26 04:58:48 -0300

    arreglo

--- scripts/store.js
+++ scripts/store.js
@@ -38,6 +38,158 @@ const products = [
     image: "https://target.scene7.com/is/image/Target/GUEST_594e74ef-1b17-4bcb-baa2-8ee7f3f791a7?wid=488&hei=488&fmt=pjpeg",
     description: "Headband with the symbol of the Hidden Leaf Village (Konoha).",
     toBuy:false
+  },
+  {
+    id: 6,
+    name: "bijus paint",
+    price: 22.99,
+    image: "https://tcustompc.com/cdn/shop/files/TailmonstersCuadro1.jpg?v=1688413815&width=713",
+    description: "Headband with the symbol of the Hidden Leaf Village (Konoha).",
+    toBuy:false
+  },
+  {
+    "id": 4,
+    "name": "Sharingan Contact Lenses",
+    "price": 19.99,
+    "image": "https://wantlens.com/wp-content/uploads/2023/06/Sba2f79861ac349cda05cb3760af5a24eL.jpg_960x960-600x600.jpg",
+    "description": "Cosmetic contact lenses designed to look like the Sharingan eye technique.",
+    "toBuy": false
+  },
+  {
+    "id": 5,
+    "name": "Konoha Headband",
+    "price": 9.99,
+    "image": "https://tcustompc.com/cdn/shop/files/Kakashi_NarutoCuadro1.jpg?v=1690313569&width=713",
+    "description": "Headband with the symbol of the Hidden Leaf Village (Konoha).",
+    "toBuy": false
+  },
+  {
+    "id": 6,
+    "name": "jakect",
+    "price": 24.99,
+    "image": "https://down-co.img.susercontent.com/file/ad507ae50d425e7d8ce2ddb9d8fd4dcf_tn",
+    "description": "Action figure of Naruto Uzumaki in his iconic outfit.",
+    "toBuy": false
+  },
+  {
+    "id": 7,
+    "name": "Sasuke Action Figure",
+    "price": 84.99,
+    "image": "https://m.media-amazon.com/images/I/71lqGwffoZL._AC_SL1500_.jpg",
+    "description": "Action figure of Sasuke Uchiha with his sword.",
+    "toBuy": false
+  },
+  {
+    "id": 8,
+    "name": "Naruto T-shirt",
+    "price": 12.99,
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBTa0hRgEAqZLLKXH_KiT4wM_9du2qlvHt-w&s",
+    "description": "Black t-shirt with a print of Naruto in action.",
+    "toBuy": false
+  },
+  {
+    "id": 9,
+    "name": "rook lee ",
+    "price": 10.99,
+    "image": "https://acdn.mitiendanube.com/stores/001/161/337/products/cci-39564045557132965-8958a1b1b2ec990bce17100918150131-1024-1024.jpg",
+    "description": "Poster of Kakashi Hatake with his Sharingan revealed.",
+    "toBuy": false
+  },
+  {
+    "id": 10,
+    "name": "Itachi Uchiha Keychain",
+    "price": 5.99,
+    "image": "https://northlandindia.com/storage/media/iqnSGecN9fGqWTUmEi76bie5mYpi69RbS69BWR0I.jpg",
+    "description": "Keychain featuring Itachi Uchiha's iconic red cloud.",
+    "toBuy": false
+  },
+  {
+    "id": 11,
+    "name": "Naruto Ramen Bowl",
+    "price": 18.99,
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNj7gJomYVdN7EroArTgt9J5aBUWsJdrGfBA&sg",
+    "description": "Ceramic ramen bowl with a Naruto Uzumaki design.",
+    "toBuy": false
+  },
+  {
+    "id": 12,
+    "name": "Kakashi tshirt",
+    "price": 29.99,
+    "image": "https://http2.mlstatic.com/D_NQ_NP_913725-MCO46091287041_052021-O.webp",
+    "description": "Backpack designed to look like Gaara's sand gourd.",
+    "toBuy": false
+  },
+  {
+    "id": 13,
+    "name": "Naruto Paintng",
+    "price": 65.99,
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNsw9FrdczO5ljWzdqQWItTl6QejJrEQEQ2g&s",
+    "description": "Metal-plated headband with the Naruto symbol.",
+    "toBuy": false
+  },
+  {
+    "id": 14,
+    "name": "glass",
+    "price": 14.99,
+    "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT1NZnRloz1ns-WsVIkV5iBLpioj39-Gz8Xha-pDHY3DhcOUfNpKV0MG3UwzU2IekmhPbouG1u1wQVZHVuk8im_7fjdHbVsA-mHBmfObrzWxR2rNBPXATcYfg&usqp=CAE",
+    "description": "Set of rings representing the Akatsuki members.",
+    "toBuy": false
+  },
+  {
+    "id": 15,
+    "name": "toy Naruto",
+    "price": "19.99",
+    "image": "https://i5.walmartimages.com/asr/25f6920f-87f2-43b1-8c47-b18c5661b547.ca3ff71f75f9aace8596ef94158aad5b.jpeg",
+    "description": "Figure of Hinata Hyuga in her combat stance.",
+    "toBuy": false
+  },
+  {
+    "id": 16,
+    "name": "coleccion Gaara",
+    "price": 39.99,
+    "image": "https://http2.mlstatic.com/D_NQ_NP_990089-MLA49336910853_032022-O.webp",
+    "description": "Complete DVD set of Naruto Shippuden series.",
+    "toBuy": false
+  },
+  {
+    "id": 17,
+    "name": "Sakura Haruno Plush",
+    "price": 14.99,
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWJkF_otwlr-3LU4R2JGzvqvPNyOmHaMiHaQ&s",
+    "description": "Plush toy of Sakura Haruno in her ninja outfit.",
+    "toBuy": false
+  },
+  {
+    "id": 18,
+    "name": "Naruto Manga Box Set",
+    "price": 99.99,
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNi9kt70-R61d4i2HECySzeRaEKS76LXCuFg&s",
+    "description": "Box set of the complete Naruto manga series.",
+    "toBuy": false
+  },
+  {
+    "id": 19,
+    "name": "Minato Namikaze Action Figure",
+    "price": 29.99,
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp5pdjdHNjKptPd20DbMs8i-9MEaBGx0vHWA&s",
+    "description": "Action figure of Minato Namikaze with his Hokage cloak.",
+    "toBuy": false
+  },
+  {
+    "id": 20,
+    "name": "Naruto Poster Set",
+    "price": 15.99,
+    "image": "https://http2.mlstatic.com/D_913945-MLA48482860264_122021-C.jpg",
+    "description": "Set of 5 posters featuring different Naruto characters.",
+    "toBuy": false
+  },
+  {
+    "id": 21,
+    "name": "Naruto Blanket",
+    "price": 34.99,
+    "image": "https://ticklemysenses.s3.amazonaws.com/wp-content/uploads/2023/04/NarutoBlanketFlannelThrowRoomDecoration_4.jpg",
+    "description": "Soft blanket with Naruto and Kurama design.",
+    "toBuy": false
   }
 ];
 
